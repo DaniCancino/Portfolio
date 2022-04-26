f850dcaedae25533469eaacb21ebd5060143a644
DaniCancino <dacr270492@gmail.com>
2022-04-26 16:01:11 -0300

    changed image of home page

--- my-portfolio/src/components/Init/Init.js
+++ my-portfolio/src/components/Init/Init.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Init.css'
-import fondo from '../../assets/DobleExpo3.png'
+import perfil from '../../assets/perfil.png'
 import { AiOutlineCloudDownload } from "react-icons/ai";
 import Typist from 'react-typist';
 import {motion} from 'framer-motion'
@@ -30,51 +30,53 @@ const Init = () =>{
             transition={{duration:0.3}}
         >
 
-            <img src={fondo} alt='fondo' className='fondo-img'></img>
-            <Typist className='hello'>
-                <Typist.Delay ms={2000} />
-                {'<h3>HELLO, WORLD.</h3>'}
-            </Typist>
+            <img src={perfil} alt='fondo' className='fondo-img'></img>
+            <div className='text-init-container'>
+                <Typist className='hello'>
+                    <Typist.Delay ms={2000} />
+                    {'<h3>HELLO, WORLD.</h3>'}
+                </Typist>
 
-            <motion.p 
-                className='name-presentation'
-                initial={{x: '-100vw'}}
-                animate={{x: 0}}
-                transition={{type: 'Spring', duration: 1, bounce: 1, stiffness: 50 }}
-            >
-                Soy Daniel Cancino.
-            </motion.p>
+                <motion.p 
+                    className='name-presentation'
+                    initial={{x: '-100vw'}}
+                    animate={{x: 0}}
+                    transition={{type: 'Spring', duration: 1, bounce: 1, stiffness: 50 }}
+                >
+                    Soy Daniel Cancino.
+                </motion.p>
 
-            <motion.p 
-                className='carrer'
-                initial={{x: '100vw'}}
-                animate={{x: 0}}
-                transition={{type: 'Spring', duration: 1, bounce: 1, stiffness: 50 }}
-            >
-                | FRONTEND DEVELOPER |
-            </motion.p>
-            <motion.a 
-                href = 'https://drive.google.com/file/d/17KjoH9VDUlm_EB6pfDAeeRDVEyhOn51o/view?usp=sharing'
-                className='Download'
-                target='_blank'
-                rel="noreferrer"
+                <motion.p 
+                    className='carrer'
+                    initial={{x: '100vw'}}
+                    animate={{x: 0}}
+                    transition={{type: 'Spring', duration: 1, bounce: 1, stiffness: 50 }}
+                >
+                    | FRONTEND DEVELOPER |
+                </motion.p>
+                <motion.a 
+                    href = 'https://drive.google.com/file/d/17KjoH9VDUlm_EB6pfDAeeRDVEyhOn51o/view?usp=sharing'
+                    className='Download'
+                    target='_blank'
+                    rel="noreferrer"
+                    variants={container}
+                    initial="hidden"
+                    animate="show"
+
+                >
+                    <AiOutlineCloudDownload />
+                    Descargar CV
+                </motion.a>
+
+                <motion.div
                 variants={container}
                 initial="hidden"
                 animate="show"
-
-            >
-                <AiOutlineCloudDownload />
-                Descargar CV
-            </motion.a>
-
-            <motion.div
-            variants={container}
-            initial="hidden"
-            animate="show"
-            className='arrow-container'
-            >
-             <Link to='/about'><FaAngleDown className= 'down-arrow'/></Link>
-            </motion.div>
+                className='arrow-container'
+                >
+                <Link to='/about'><FaAngleDown className= 'down-arrow'/></Link>
+                </motion.div>
+            </div>
         </motion.div>
     )
 }
